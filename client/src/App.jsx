420a7f90de9afa199f4b4651b1ca72184a1aa9b0
Donvictory <donvictoryadewumi4@gmail.com>
2026-02-20 22:06:19 +0100

    refactor: relocate client-side application files into a dedicated `client` directory.

---
+++ client/src/App.jsx
@@ -0,0 +1,24 @@
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import Dashboard from "./pages/Dashboard";
+import Admin from "./pages/Admin";
+import ProtectedRoute from "./routes/ProtectedRoute";
+import JobApplication from "./pages/JobApplication";
+
+export default function App() {
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/register" element={<Register />} />
+      <Route path="/job-application" element={<JobApplication />} />
+
+      <Route element={<ProtectedRoute />}>
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/admin" element={<Admin />} />
+      </Route>
+    </Routes>
+  );
+}
